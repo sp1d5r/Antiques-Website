c38d68c06fbe779e6f270f17dc4e6f9c697c0537
Elijah <aa1719@imperial.ac.uk>
2023-09-20 17:46:11 +0100

    Frontend: First version of the footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import FacebookIcon from "../assets/icons/facebook.svg";
+import InstagramIcon from "../assets/icons/instagram.svg";
+import PhoneIcon from "../assets/icons/phone-solid.svg";
+import EmailIcon from "../assets/icons/envelope-solid.svg";
+
 
 const Footer = () => {
     return <div className={"w-full min-h-[30vh] border-t bg-white flex justify-between items-start p-16"}>
@@ -11,6 +16,17 @@ const Footer = () => {
 
         <div>
             <p>Links</p>
+            <a>Contact Us</a>
+            <div>
+                <p>Mobile: 020 7381 1601</p>
+                <p>Email:  Info@matthewupham.com</p>
+                <div className={"flex gap-2"}>
+                    <img className={"h-8"} src={FacebookIcon} alt={"https://www.facebook.com/uphamantiques/"} />
+                    <img className={"h-8"} src={InstagramIcon} alt={"https://www.instagram.com/matthew_upham_antiques/"} />
+                    <img className={"h-8"} src={PhoneIcon} alt={"https://www.facebook.com/uphamantiques/"} />
+                    <img className={"h-8"} src={EmailIcon} alt={"https://www.facebook.com/uphamantiques/"} />
+                </div>
+            </div>
         </div>
 
     </div>
